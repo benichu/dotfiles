41106fd41d9231354bcaa6a50e739d2c3df2c17d
Ben Thouret <ben@2ret.com>
2012-10-02 17:35:08 -0400

    Working on the install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,13 +12,14 @@ echo
 
 ########## Variables
 
-dir=~/dotfiles        # dotfiles directory
+endpath="$HOME/dotfiles" # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
 # TODO: in progress
 files="bashrc gitattributes tmux.conf gemrc ackrc inputrc gitignore"        # list of files/folders to symlink in homedir
 
 ##########
 
+# TODO: Better backup of older dotfiles (timestamp)
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
@@ -26,7 +27,11 @@ echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
-cd $dir
+cd $endpath
+if [ -e $endpath/.git ]; then
+  echo "getting the latest version of your dotfiles\n"
+  cd $endpath && git pull
+fi
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
@@ -34,5 +39,5 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $endpath/$file ~/.$file
 done
