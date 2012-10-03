8782faed957433023c6e7da7d4abbe3d739ad476
Ben Thouret <ben@2ret.com>
2012-10-03 10:37:04 -0400

    Fix bootsrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,19 +31,20 @@ lnif() {
 
 endpath="$HOME/dotfiles" # dotfiles directory
 today=`date +%F-%T`
+backupdir="$HOME/old.dotfiles.$today"
 
 # list of files/folders to symlink in homedir
 dotfiles="bashrc gitattributes tmux.conf gemrc ackrc inputrc gitignore"
 
 ##########
 
-echo "thanks for installing this dotfile\n"
+echo "thanks for installing this dotfile"
 
 # change to the dotfiles directory
-echo "changing to the $endpath directory\n"
+echo "changing to the $endpath directory"
 cd $endpath
 if [ -e $endpath/.git ]; then
-  echo "getting the latest version of your dotfiles\n"
+  echo "getting the latest version of your dotfiles"
   cd $endpath && git pull
   echo "...update done"
 fi
@@ -51,9 +52,9 @@ fi
 # move any existing dotfiles in homedir to a timestamped backup directory, then create symlinks
 for file in $dotfiles; do
     # Backup existing dotfiles stuff
-    echo "backing up any existing dotfiles in ~\n"
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$dotfiles ~/old.dotfiles.$today
+    echo "backing up any existing dotfiles to $backupdir"
+    mkdir $backupdir
+    mv ~/.$dotfiles $backupdir
     echo "Creating symlink to $file in home directory."
     lnif $endpath/$file ~/.$file
     echo "...symlink done"
